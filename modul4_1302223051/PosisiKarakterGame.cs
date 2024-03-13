003aef1497ab5c25106b06bb44d459fdc54d30b9
unknown <rickyrenaldi100@gmail.com>
2024-03-13 08:13:16 +0700

    menambahkan class PosisiKarakterGame

---
+++ modul4_1302223051/PosisiKarakterGame.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_1302223051
+{
+    internal class PosisiKarakterGame
+    {
+        enum Posisi { Jongkok, Berdiri, Tengkurap, Terbang };
+        enum Tombol { TombolW, TombolS, TombolX };
+
+        public static void Main()
+        {
+            Console.Write("Tekan Tombol");
+
+            string perintah = Console.ReadLine();
+            PosisiKarakterGame.Posisi posisi = PosisiKarakterGame.Posisi.Berdiri;
+
+            while (perintah != "")
+            {
+                switch(posisi)
+                {
+                    /**case PosisiKarakterGame.Posisi.Berdiri:
+                        if (perintah == "W")
+                        {
+                            posisi = Posisi.Terbang;
+                        }
+                */} 
+            }
+        }
+    }
+}
